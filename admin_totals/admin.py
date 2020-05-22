0412955b706bf0c46f9b19ffe6e965bcc1a3e73c
Heronmb <heronmb@gmail.com>
2020-05-22 11:43:53 -0300

    Update admin.py

--- admin_totals/admin.py
+++ admin_totals/admin.py
@@ -4,7 +4,7 @@ from __future__ import division, print_function, unicode_literals
 from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 
-
+'''
 class ChangeListTotals(ChangeList):
     def get_results(self, *args, **kwargs):
         super(ChangeListTotals, self).get_results(*args, **kwargs)
@@ -15,6 +15,30 @@ class ChangeListTotals(ChangeList):
                 if field in list_totals:
                     self.aggregations.append(
                         self.result_list.aggregate(agg=list_totals[field](field))['agg'])
+                else:
+                    self.aggregations.append('')
+
+'''
+# Heron
+class ChangeListTotals(ChangeList):
+    logger.warning('on the changelist ')
+    def get_results(self, *args, **kwargs):
+        super(ChangeListTotals, self).get_results(*args, **kwargs)
+        if hasattr(self.model_admin, 'list_totals'):
+            logger.warning('has list_totals ' + str(self.model_admin.list_totals))
+            self.aggregations = []
+            list_totals = dict(self.model_admin.list_totals)
+            logger.warning('list_totals: ' + str(list_totals))
+            for field in self.list_display:
+                logger.warning('filed list_display: ' + str(field))
+                if field in list_totals:
+                    logger.warning('filed: ' + str(field))
+                    logger.warning(self.result_list)
+                    self.aggregations.append(
+                        self.result_list.aggregate(agg=list_totals[field](field))['agg'])
+                    logger.warning('list_totals[field](field))[agg]:')
+                    logger.warning(self.aggregations)
+                    #logger.warning(list_totals[field](field)['agg'])
                 else:
                     self.aggregations.append('')
 
